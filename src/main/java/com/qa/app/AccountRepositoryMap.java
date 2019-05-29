3ab4f72dd225a66990d73c788f9b86c5e957bf8c
TGilbert <Thomas.Gilbert@academytrainee.com>
2019-05-29 10:06:16 +0100

    Adhered to SOLID principles, added interface AccountRepository

--- src/main/java/com/qa/app/AccountRepositoryMap.java
+++ src/main/java/com/qa/app/AccountRepositoryMap.java
@@ -2,13 +2,15 @@ package com.qa.app;
 
 import java.util.HashMap;
 
-public class AccountRepositoryMap {
+public class AccountRepositoryMap implements AccountRepository{
 	public HashMap<Integer, Account> maps = new HashMap<Integer, Account>();
 
 	public void add(Account account) {
 		maps.put(account.getId(), account);
 	}
-	public Account retrieve(Integer id) {
+	
+	public Account retrieve(int id) {
+		
 		return maps.get(id);
 	}
 }
